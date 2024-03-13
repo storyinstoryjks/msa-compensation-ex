f51d3e515e298d33545fb50f2280c81c274232f1
KimKyusoo <123912988+kyusooK@users.noreply.github.com>
2024-03-13 09:42:49 +0900

    commit

---
+++ inventory/src/main/java/compensation/domain/OutOfStock.java
@@ -0,0 +1,25 @@
+package compensation.domain;
+
+import compensation.domain.*;
+import compensation.infra.AbstractEvent;
+import java.time.LocalDate;
+import java.util.*;
+import lombok.*;
+
+//<<< DDD / Domain Event
+@Data
+@ToString
+public class OutOfStock extends AbstractEvent {
+
+    private Long id;
+    private Long orderId;
+
+    public OutOfStock(Inventory aggregate) {
+        super(aggregate);
+    }
+
+    public OutOfStock() {
+        super();
+    }
+}
+//>>> DDD / Domain Event
